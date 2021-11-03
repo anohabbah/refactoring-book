4a3f4bdb1f7057d98d96194a223d7f851d495e52
Abbah Anoh <aanoh.timwi@gmail.com>
2021-11-03 11:04:56 +0100

    create intermediate data object and pass it to statement printing func

--- statement.js
+++ statement.js
@@ -7,10 +7,11 @@ const plays = require("./plays.json");
  * @return {string}
  */
 function statement(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
